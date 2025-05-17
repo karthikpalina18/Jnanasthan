ab11af00a736259ab6c0f85150be6883ef176f8f
Palina Karthik <karthikpalina18@gmail.com>
2025-05-17 18:37:55 +0530

    Fix line endings

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,16 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
-// import App from './App';
-// import './index.css'; 
-
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import { ChatProvider } from './context/ChatContext'; // ✅ Import the context provider
+import { ChatProvider } from './context/ChatContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChatProvider> {/* ✅ Wrap App with ChatProvider */}
-      <BrowserRouter>
+    <ChatProvider>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ChatProvider>
   </React.StrictMode>
 );
