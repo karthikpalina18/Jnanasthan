9a6c077a98a32e6deb9441a5a419519ac2d37673
Palina Karthik <karthikpalina18@gmail.com>
2025-05-17 19:43:17 +0530

    Prepare production build for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/Jnanasthan-trail/',
+export default defineConfig({
+  base: '/Jnanasthan-trail/',
   plugins: [react()],
-}))
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+})
