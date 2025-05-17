bef8391e6da758537f65683d41dda8e2ce1a4cca
Palina Karthik <karthikpalina18@gmail.com>
2025-05-17 18:53:02 +0530

    Updated deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Jnanasthan-trail/',  // <--- Add trailing slash here!
+  base: '/Jnanasthan-trail/',
   plugins: [react()],
 })
