f19d3359fb81a89d590593da581a63fa13389f62
Palina Karthik <karthikpalina18@gmail.com>
2025-05-17 19:21:24 +0530

    Setup Vite React app with routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';
-import { ChatProvider } from './context/ChatContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChatProvider>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </ChatProvider>
+    <BrowserRouter basename="/Jnanasthan-trail/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
