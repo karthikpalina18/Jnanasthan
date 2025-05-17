7a8a749d5b7d6224fc5badc64c66c7d18007c3a9
Palina Karthik <karthikpalina18@gmail.com>
2025-05-17 19:30:00 +0530

    Your commit message

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
+import { ChatProvider } from './context/ChatContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Jnanasthan-trail/">
-      <App />
-    </BrowserRouter>
+    <ChatProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </ChatProvider>
   </React.StrictMode>
 );
